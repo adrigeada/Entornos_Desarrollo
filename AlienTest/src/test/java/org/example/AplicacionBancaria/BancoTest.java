770fc6ebde50954e5f18bda57034d668ff6858d5
DESKTOP-MVTPKEL\Adrian Geada <adrigeada@gmail.com>
2026-03-31 14:49:55 +0200

    **

---
+++ AlienTest/src/test/java/org/example/AplicacionBancaria/BancoTest.java
@@ -0,0 +1,21 @@
+package org.example.AplicacionBancaria;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BancoTest {
+
+    @Test
+    void depositar() {
+
+    }
+
+    @Test
+    void retirar() {
+    }
+
+    @Test
+    void consultarSaldo() {
+    }
+}
